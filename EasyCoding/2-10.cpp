0f1eff2055b760697e07bbfc2a2b034805600344
Molyuu <mi@molyuu.cyou>
2023-07-01 16:36:36 +0800

    我咖啡滴我书上了我不玩了

---
+++ EasyCoding/2-10.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, d, e, f, min;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    min = c * 60 + d - a * 60 - b ;
+    f = min % 60;
+    e = (min - f) / 60;
+    printf("%d %d", e, f);
+    return 0;
+}
