373aac41f1e3ecc57fe4ae6868825184aef3b4c8
Molyuu <mi@molyuu.cyou>
2023-07-01 16:37:56 +0800

    哎呀忘记格式化了

--- EasyCoding/2-10.cpp
+++ EasyCoding/2-10.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a, b, c, d, e, f, min;
     scanf("%d %d %d %d", &a, &b, &c, &d);
-    min = c * 60 + d - a * 60 - b ;
+    min = c * 60 + d - a * 60 - b;
     f = min % 60;
     e = (min - f) / 60;
     printf("%d %d", e, f);
