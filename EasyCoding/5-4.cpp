41e5baceeaf0c88cac01fe99697b747058902317
Molyuu <mi@molyuu.cyou>
2023-07-28 18:32:08 +0800

    太好玩了 OI

--- EasyCoding/5-4.cpp
+++ EasyCoding/5-4.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-    int tree[10010] = {0};
+    int tree[10010] = {0}, l, m, a = 0, b = 0, ans = 0;
+    cin >> l >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        for (int j = a; j <= b; j++) {
+            tree[j] = 1;
+        }
+    }
+
+    for (int i = 0; i <= l; i++) {
+        if (tree[i] == 0) {
+            ans++;
+        }
+    }
+
+    cout << ans;
+
     return 0;
 }
