4449f1dfaee6b7ef7ea26447ee70b94a64ae4a94
Molyuu <mi@molyuu.cyou>
2023-12-03 15:55:12 +0800

    小朋友的代码

--- club/problemset/dict.cpp
+++ club/problemset/dict.cpp
@@ -1,60 +1,57 @@
-// 其实这个题没必要那么复杂（（
-// 用一些字符串的函数就可以很方便的解决这个问题了
-
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int minDistance(string word1, string word2) {
-    int n = word1.length();
-    int m = word2.length();
+const int N = 5e1 + 10;
+int n, q;
+string s[N];
 
-    if (n * m == 0)
-        return n + m;
-
-    vector<vector<int>> D(n + 1, vector<int>(m + 1));
-
-    for (int i = 0; i < n + 1; i++) {
-        D[i][0] = i;
-    }
-    for (int j = 0; j < m + 1; j++) {
-        D[0][j] = j;
-    }
-
-    for (int i = 1; i < n + 1; i++) {
-        for (int j = 1; j < m + 1; j++) {
-            int left = D[i - 1][j] + 1;
-            int down = D[i][j - 1] + 1;
-            int left_down = D[i - 1][j - 1];
-            if (word1[i - 1] != word2[j - 1])
-                left_down += 1;
-            D[i][j] = min(left, min(down, left_down));
-        }
-    }
-    return D[n][m];
-}
-
-int main() {
-    int n, q;
-    cin >> n >> q;
-    vector<string> dict(n), word(q), results(q);
-    for (int i = 0; i < n; i++)
-        cin >> dict[i];
-    for (int i = 0; i < q; i++)
-        cin >> word[i];
-
-    for (int i = 0; i < q; i++) {
-        results[i] = "No";
-        for (int j = 0; j < n; j++) {
-            if (minDistance(word[i], dict[j]) <= 1) {
-                results[i] = dict[j];
+int main(void) {
+    scanf("%d %d", &n, &q);
+    for (int i = 1; i <= n; i++)
+        cin >> s[i];
+    while (q--) {
+        int flag = 0;
+        string s1, s2;
+        cin >> s2;
+        for (int i = 1; i <= n; i++) {
+            s1 = s[i];
+            int l1 = s1.size(), l2 = s2.size();
+            if (s1 == s2) {
+                flag = 1;
+                cout << s1 << '\n';
+                break;
+            }
+            if (abs(l1 - l2) > 1)
+                continue;
+            int cnt = 0;
+            for (int i = 0, j = 0;; i++, j++) {
+                if (s1[i] ^ s2[j]) {
+                    cnt++;
+                    if (cnt > 1)
+                        break;
+                    if (l1 > l2)
+                        i++;
+                    else if (l1 < l2)
+                        j++;
+                    else
+                        i++, j++;
+                }
+                if (i == l1 && j == l2) {
+                    flag = 1;
+                    cout << s1 << '\n';
+                    break;
+                }
+                if (i == l1 - 1 && j == l2 - 1) {
+                    if (s1[i] == s2[j]) {
+                        flag = 1;
+                        cout << s1 << '\n';
+                    }
+                    break;
+                }
             }
         }
-    }
-
-    for (int i = 0; i < q; i++) {
-        cout << results[i] << endl;
+        if (!flag)
+            puts("No");
     }
     return 0;
 }
