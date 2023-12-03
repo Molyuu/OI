4b5012519028709420e13cca1efd70b1f55a137d
Molyuu <mi@molyuu.cyou>
2023-12-03 15:53:52 +0800

    T1 modified

--- club/problemset/princess.cpp
+++ club/problemset/princess.cpp
@@ -1,23 +1,22 @@
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
 
+double a[11], maxx = -2e9;
+int ans;
+
 int main() {
-    int a[15];
-    a[0] = 1, a[10] = 10000;
-    scanf("%d %d %d %d %d %d %d %d %d", &a[1], &a[2], &a[3], &a[4], &a[5],
-          &a[6], &a[7], &a[8], &a[9]);
-
-    int max_change_year = 1;
-    double max_change_value = 0.0;
-
-    for (int i = 0; i <= 10; i++) {
-        double tmp1 = a[i], tmp2 = a[i - 1];
-        double change_value = tmp1 / tmp2;
-        if (change_value >= max_change_value) {
-            max_change_value = change_value;
-            max_change_year = i;
+    a[0] = 1;
+    for (int i = 1; i <= 9; i++) {
+        cin >> a[i];
+    }
+    a[10] = 10000;
+    for (int i = 1; i <= 10; i++) {
+        double x = a[i] / a[i - 1];
+        if (x > maxx) {
+            maxx = x;
+            ans = i;
         }
     }
-    printf("%d", max_change_year);
+    cout << ans;
     return 0;
 }
